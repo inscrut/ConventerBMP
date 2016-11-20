132c66e8bff15d163f29d68d62d9edf2b589bdc2
Kirill <kirr2@gf.tom.ru>
2016-11-20 21:13:32 +0700

    Release 1.0;

--- ConventerBMP/Program.cs
+++ ConventerBMP/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
 
 namespace ConventerBMP
 {
@@ -10,6 +12,11 @@ namespace ConventerBMP
     {
         static void Main(string[] args)
         {
+            foreach (var item in args)
+            {
+                Image img = new Bitmap(item);
+                img.Save(Path.GetFileNameWithoutExtension(item) + ".bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+            }
         }
     }
 }
